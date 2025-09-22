6d5db1289c838b494a9b68d8b402d46cc01ede07
Michael Tase <tasemgt@gmail.com>
2025-09-22 23:38:19 +0100

    build gold layer and its views (Fact and Dim tables)

--- test_proc.sql
+++ test_proc.sql
@@ -1,5 +1,11 @@
 
 EXEC bronze.load_bronze_data; -- Load data into Bronze tables
 
-
 EXEC silver.load_silver_data; -- Load data into Bronze tables
+
+
+Select distinct gender from gold.dim_customers;
+
+Select * from gold.dim_products;
+
+Select * from gold.fact_sales;
